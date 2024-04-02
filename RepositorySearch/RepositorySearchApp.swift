f7a4cefd6658eedbad0d6a819dcad0e2f78b5780
kroli98 <roli25321@gmail.com>
2024-04-02 20:42:42 +0200

    Initial Commit

---
+++ RepositorySearch/RepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositorySearchApp.swift
+//  RepositorySearch
+//
+//  Created by Kiss Roland on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
