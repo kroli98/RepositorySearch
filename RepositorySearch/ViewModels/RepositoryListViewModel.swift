63e759d2e2523a35a36982ded1a647f5b4e45901
kroli98 <roli25321@gmail.com>
2024-04-04 23:45:06 +0200

    renaming, correcting fetch logic

--- RepositorySearch/ViewModels/RepositoryListViewModel.swift
+++ RepositorySearch/ViewModels/RepositoryListViewModel.swift
@@ -7,24 +7,48 @@
 
 import Foundation
 
-class ListElementViewModel: ObservableObject {
+class RepositoryListViewModel: ObservableObject {
     
     var query: String
+    var isFirstLoad = true
     @Published var repos: [RepositoryModel] = []
+    @Published var error: NetworkError?
     @Published var isLoading = false
+    @Published var showErrorAlert: Bool
+    
     
     init(query: String) {
+        
         self.query = query
+        self.showErrorAlert = false
+        loadRepos()
+        
     }
     
-    func getRepos() {
+    func loadRepos() {
+        
+        guard isFirstLoad else { return }
+        isFirstLoad = false
+        
         isLoading = true
         
-        WebService().fetchRepositories(query: query) { (fetchedRepos: [RepositoryModel]?) in
-               if let fetchedRepos = fetchedRepos {
-                   self.repos = fetchedRepos
-                   self.isLoading = false
-               }
-           }
-       }
+        
+        WebService().fetchRepositories(query: query) { [weak self] result in
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+                
+                self?.isLoading = false
+                switch result {
+                case .success(let repos):
+                    self?.repos = repos
+                case .failure(let error):
+                    self?.error = error
+                    self?.showErrorAlert = true
+                }
+                
+            }
+            
+            
+        }
+    }
 }
