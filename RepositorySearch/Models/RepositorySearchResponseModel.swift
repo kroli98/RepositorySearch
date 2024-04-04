05b37b9f3e63e1e43f08768323928ab996a42791
kroli98 <roli25321@gmail.com>
2024-04-04 23:42:42 +0200

    removing unnecessary fields

--- RepositorySearch/Models/RepositorySearchResponseModel.swift
+++ RepositorySearch/Models/RepositorySearchResponseModel.swift
@@ -7,13 +7,7 @@
 
 import Foundation
 struct RepositorySearchResponse: Codable {
-    let totalCount: Int
-    let incompleteResults: Bool
+  
     let items: [RepositoryModel]
 
-    enum CodingKeys: String, CodingKey {
-        case totalCount = "total_count"
-        case incompleteResults = "incomplete_results"
-        case items
-    }
 }
